07321a933c50e9b507c8d68ecc8551bb3ae42ac0
abols360 <aleksis.abols@va.lv>
2022-05-20 01:32:38 +0300

    Fixed animController

--- Assets/Spider.cs
+++ Assets/Spider.cs
@@ -9,16 +9,33 @@ public class Spider : Character
 
     [SerializeField] float damagePerSecond = 20f;
 
+    Animator _animator;
+
+ 
+
+    private void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+      //  CurrentState = State.Idle;
+    }
+
     private void Update()
     {
         Vector3 heroPosition = GameManager.instance.Hero.transform.position;
         float distanceToHero = Vector3.Distance(transform.position, heroPosition);
 
-     //   Debug.Log("distance to hero" + distanceToHero);
+        //   Debug.Log("distance to hero" + distanceToHero);
 
-        if(distanceToHero < closeEnoughDistance)
+          if (distanceToHero < closeEnoughDistance)
         {
             GameManager.instance.Hero.AddDamage(Time.deltaTime * damagePerSecond);
+            // CurrentState = State.Attacking;
+            _animator.SetBool("isAttacking", true);
+        }
+        else
+        {
+            _animator.SetBool("isAttacking", false);
         }
     }
+ 
 }
