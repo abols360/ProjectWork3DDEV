0fe72b947025022d7ad46abbfddf390b7c4292a7
abols360 <aleksis.abols@va.lv>
2022-05-31 17:27:19 +0300

    update spider attack logic and fix attack sound

--- Assets/Spider.cs
+++ Assets/Spider.cs
@@ -10,13 +10,14 @@ public class Spider : Character
 
     [SerializeField] float damagePerSecond = 20f;
     [SerializeField] Transform sightOrgin;
+    [SerializeField] LayerMask sightMask = ~0;
 
     Animator _animator;
 
     NavMeshAgent _navMeshAgent;
     AudioSource _audioSource;
 
-
+ public Vector3 HeroPosition => GameManager.instance.Hero.transform.position + Vector3.up * 0.5f;
     // bool isIdle = false;
     // bool isAttacking = false;
 
@@ -75,8 +76,7 @@ public class Spider : Character
             {
                 _navMeshAgent.velocity = Vector3.zero;
                 // GameManager.instance.Hero.AddDamage(Time.deltaTime * damagePerSecond);
-                _audioSource.Play(); //SKAN tikai tad, ja ieiet un iezie no uzbrukuma rādiusa. 
-                Debug.Log("SOUND");
+               
                 CurrentState = State.Attacking;
           
             }
@@ -116,13 +116,37 @@ public class Spider : Character
     public void Bite(){
           if (CurrentState == State.Attacking){
               GameManager.instance.Hero.AddDamage(damagePerSecond);
+               _audioSource.Play(); //SKAN tikai tad, ja ieiet un iezie no uzbrukuma rādiusa. 
+                Debug.Log("SOUND");
           }  
     }
 
     bool CanSeePlayer(){
-      //  Physics.Raycast()
+        Vector3 heroPosition = HeroPosition;
+        bool canSee = false;
+        Vector3 hitPos = heroPosition;
+
+        Vector3 dir = (heroPosition - sightOrgin.position).normalized;
+
+        if (Physics.Raycast(sightOrgin.position, dir, out RaycastHit hit, 30f, sightMask))
+        {
+            if (hit.transform.gameObject.CompareTag("Player")) canSee = true;
+
+            hitPos = hit.point;
+
+            Debug.DrawLine(
+                sightOrgin.position,
+                hit.point,
+                canSee ? Color.green : Color.yellow
+            );
+        }
+        else
+        {
+            Debug.DrawRay(sightOrgin.position, dir, Color.red);
+        }
+        return canSee;
         
-        return false;
+       
         }
     
  
